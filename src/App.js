3cc741b114d2e73218a331599d76e240a8787f63
Eric Laursen <ericjlaursen@gmail.com>
2019-06-17 10:48:05 -0400

    init todo commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Title from './components/Title'
+import ToDo from './ToDo'
+
+export default ToDo;
+ReactDOM.render(<ToDo />, document.getElementById("app"));
