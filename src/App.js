a1ee6626ce26388cb24e03f6ec667fc36d8116d9
Eric Laursen <ericjlaursen@gmail.com>
2019-06-17 11:00:14 -0400

    imports rtg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 import cx from 'classnames';
 import './main.css';
 
